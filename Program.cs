704105902772c35dcd4d3376b0da39b6f6f37af3
Austin Warrick <austinwarrick@gmail.com>
2023-11-29 02:15:03 -0600

    completed lesson

--- Program.cs
+++ Program.cs
@@ -70,6 +70,7 @@ Console.WriteLine(greeting);
 
 
 string choice = null;
+
 while (choice != "0")
 {
     Console.Clear(); // Clears the console before displaying the menu
@@ -79,10 +80,11 @@ while (choice != "0")
                         1. Display All Products
                         2. Add a Product
                         3. Delete a Product
-                        4. Edit a Product");
+                        4. Edit a Product
+                        5. View Products by Type"); // New option for viewing products by type
 
     choice = Console.ReadLine();
-    
+
     if (choice == "0")
     {
         Console.WriteLine("Goodbye!");
@@ -95,17 +97,22 @@ while (choice != "0")
     else if (choice == "2")
     {
         Console.Clear(); // Clears the console before displaying the message
-        throw new NotImplementedException("Add a Product");
+        AddProduct();
     }
     else if (choice == "3")
     {
         Console.Clear(); // Clears the console before displaying the message
-        throw new NotImplementedException("Delete A Product");
+        DeleteProduct();
     }
     else if (choice == "4")
     {
         Console.Clear(); // Clears the console before displaying the message
-        throw new NotImplementedException("Edit A Product");
+        UpdateProduct();
+    }
+    else if (choice == "5") // New option for viewing products by type
+    {
+        Console.Clear(); // Clears the console before displaying the message
+        ViewProductsByType();
     }
     else
     {
@@ -115,9 +122,10 @@ while (choice != "0")
     }
 }
 
+
 string ProductDetails(Product product)
 {
-    string productString = $"{product.Name} is sold for ${product.Price} and is {(product.IsAvailable ? "available" : $"not available")} at this time.";
+    string productString = $"{product.Name} is priced at ${product.Price} and is {(product.IsAvailable ? "available" : $"not available")} at this time.";
     return productString;
 }
 
@@ -130,3 +138,201 @@ void DisplayAllProducts()
     Console.WriteLine("press any key to continue...");
     Console.ReadKey();
 }
+void AddProduct()
+{
+    Console.WriteLine("Please enter new product details.");
+
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Price (use exactly two decimal points): ");
+    decimal price;
+    while (!decimal.TryParse(Console.ReadLine(), out price) || price < 0)
+    {
+        Console.WriteLine("Invalid response. Price should be a positive number with exactly two decimal points.");
+        Console.Write("Price: ");
+    }
+
+
+
+    Console.WriteLine("Available Product Types:");
+    for (int i = 0; i < productTypes.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {productTypes[i].Name}");
+    }
+
+    Console.Write("Choose the corresponding Product Type (enter the corresponding number): ");
+    int productTypeId;
+    while (!int.TryParse(Console.ReadLine(), out productTypeId) || productTypeId < 1 || productTypeId > productTypes.Count)
+    {
+        Console.WriteLine("Invalid input. Please choose a valid Product Type.");
+        Console.Write("Choose the corresponding Product Type (enter the corresponding number): ");
+    }
+
+    int newProductId = products.Count > 0 ? products.Max(p => p.ProductId) + 1 : 1;
+
+    Product newProduct = new Product()
+    {
+        ProductId = newProductId,
+        Name = name,
+        Price = price,
+        IsAvailable = true,
+        ProductTypeId = productTypeId
+    };
+
+    products.Add(newProduct);
+
+    Console.WriteLine($"The Product '{name}' has been posted successfully!");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+
+void DeleteProduct()
+{
+    Console.WriteLine("Enter the ID of the product you want to delete:");
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+
+    try
+    {
+        int ProductNumber = int.Parse(Console.ReadLine());
+        if (ProductNumber >= 1 && ProductNumber <= products.Count)
+        {
+            products.RemoveAt(ProductNumber -  1);
+            Console.WriteLine("Product was successfully deleted.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid plant number. Please choose a valid number.");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+
+void UpdateProduct()
+{
+    Console.WriteLine("Enter the ID of the product you want to update:");
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+
+    try
+    {
+        int productNumber = int.Parse(Console.ReadLine());
+        if (productNumber >= 1 && productNumber <= products.Count)
+        {
+            // Get the product to be updated
+            Product productToUpdate = products[productNumber - 1];
+
+            // Update product name
+            Console.Write("Enter the new name for the product: ");
+            string newName = Console.ReadLine();
+            productToUpdate.Name = newName;
+
+            // Update product price
+            Console.Write("Enter the new price for the product: $");
+            decimal newPrice;
+            while (!decimal.TryParse(Console.ReadLine(), out newPrice) || newPrice < 0)
+            {
+                Console.WriteLine("Invalid input. Price should be a positive number.");
+                Console.Write("Enter the new price for the product: $");
+            }
+            productToUpdate.Price = newPrice;
+
+            // Update product type
+            Console.WriteLine("Choose the corresponding product type:");
+            DisplayProductTypes();
+            int newProductTypeId;
+            while (!int.TryParse(Console.ReadLine(), out newProductTypeId) || !IsValidProductTypeId(newProductTypeId))
+            {
+                Console.WriteLine("Invalid input. Please choose a valid product type.");
+                Console.WriteLine("Choose the corresponding product type:");
+            }
+            productToUpdate.ProductTypeId = newProductTypeId;
+
+            Console.WriteLine("Product was successfully updated.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid product number. Please choose a valid number.");
+        }
+    }
+    finally
+    {
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
+
+bool IsValidProductTypeId(int productTypeId)
+{
+    // Check if the provided product type ID is valid
+    return productTypes.Any(pt => pt.ProductTypeId == productTypeId);
+}
+
+void DisplayProductTypes()
+{
+    // Display available product types
+    foreach (var productType in productTypes)
+    {
+        Console.WriteLine($"{productType.ProductTypeId}. {productType.Name}");
+    }
+}
+
+
+void DisplayProductsByType(int productTypeId)
+{
+    Console.WriteLine($"Products of Type {productTypeId}:");
+    
+    // Filter products based on the selected product type
+    var filteredProducts = products.Where(p => p.ProductTypeId == productTypeId).ToList();
+
+    if (filteredProducts.Any())
+    {
+        for (int i = 0; i < filteredProducts.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {ProductDetails(filteredProducts[i])}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("No products found for the selected type.");
+    }
+
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+void ViewProductsByType()
+{
+    Console.WriteLine("Product Types:");
+    for (int i = 0; i < productTypes.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {productTypes[i].Name}");
+    }
+
+    Console.WriteLine("Enter the product type ID to view products:");
+    if (int.TryParse(Console.ReadLine(), out int productTypeId))
+    {
+        DisplayProductsByType(productTypeId);
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number.");
+    }
+}
+
